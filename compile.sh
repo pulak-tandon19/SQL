e93f863f9d43ae1a1b36b40243461187c8775a27
pulak-tandon19 <pulaktandon2000@gmail.com>
2026-01-15 02:11:10 +0530

    changes in compile file

--- compile.sh
+++ compile.sh
@@ -8,3 +8,10 @@ lex SQLParser/Parser.l
 
 echo "--- Compiling lex.yy.c ---"
 g++ -g -c lex.yy.c -o lex.yy.o
+
+echo "--- Compiling SQLParserMain.c ---"
+g++ -g -c SQLParser/SQLParserMain.c -o SQLParserMain.o
+
+echo "--- Compiling SQLCreateParserCFG.c ---"
+g++ -g -c SQLParser/SQLCreateParserCFG.c -o SQLCreateParserCFG.o
+
