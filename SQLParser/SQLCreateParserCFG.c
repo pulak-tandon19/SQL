b2742d2b541c75641af103a55a788045374a56de
pulak-tandon19 <pulaktandon2000@gmail.com>
2026-01-15 02:10:59 +0530

    initial parser for sql parser and sql create parser

---
+++ SQLParser/SQLCreateParserCFG.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+#include<assert.h>
+
+#include "SqlEnums.h"
+#include "ParserExport.h"
+
+parse_rc_t create_query_parser() {
+    parse_init();
+}
