b9fb6f539522aeb4803730a98831738dc6e48d72
pulak-tandon19 <pulaktandon2000@gmail.com>
2026-01-15 03:35:12 +0530

    adding lex_curr_token extern

--- SQLParser/ParserExport.h
+++ SQLParser/ParserExport.h
@@ -36,6 +36,7 @@ extern "C" int yylex();
 extern char lex_buffer[MAX_MEXPR_LEN];
 extern stack_t undo_stack;
 extern char *curr_ptr;
+extern char *lex_curr_token;
 extern int cyylex();
 extern void yyrewind(int n);
 extern void parser_stack_reset();
