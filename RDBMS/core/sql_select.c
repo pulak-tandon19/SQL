ceba6e66363f6b42510d2474ba583e92f975f492
pulak-tandon19 <pulaktandon2000@gmail.com>
2026-02-08 03:17:52 +0530

    implementation of select query processing

--- RDBMS/core/sql_select.c
+++ RDBMS/core/sql_select.c
@@ -12,3 +12,24 @@ bool
 sql_query_initialize_select_column_list  (qep_struct_t *qep, BPlusTree_t *tcatalog) {   
     return true;
 }
+
+
+static void 
+debug_print_joined_row(joined_row_t * joined_row) {
+    int i;
+    void *rec;
+
+    for (int i = 0; i < joined_row->size; i++) {
+        rec = joined_row->rec_array[i];
+        printf("%s", (char *)rec);
+    }
+}
+
+void
+sql_process_select_query (qep_struct_t *qep) {
+
+    while (qep_execute_join(qep)) {
+        debug_print_joined_row(qep->joined_row_tmplate);
+        printf("\n");
+    }
+}
